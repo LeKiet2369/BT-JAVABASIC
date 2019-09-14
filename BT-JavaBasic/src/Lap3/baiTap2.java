72fde0a4a1c062d8b43fddc0bc1bcb2d5afd14e7
LeKiet2369 <55068146+LeKiet2369@users.noreply.github.com>
2019-09-14 22:10:27 +0700

    lap 3
    bo sung bai tap 2

--- BT-JavaBasic/src/Lap3/baiTap2.java
+++ BT-JavaBasic/src/Lap3/baiTap2.java
@@ -5,10 +5,20 @@
  */
 package Lap3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
 public class baiTap2 {
+    public static void main(String[] args){
+        for(int i = 2; i <=9; i++){
+            for(int j = 1 ; j <= 9; j++){
+                System.out.print( j + " x " + i + " = " +i*j+ "\t");
+            }
+            System.out.println();
+        }
+    }
     
 }
